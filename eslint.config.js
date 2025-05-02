08803643c14bd51f12741e152293df2027cece62
Devanshu Prakash <devanshuprakash2301@gmail.com>
2025-05-02 11:58:36 +0530

    fbdd

--- eslint.config.js
+++ eslint.config.js
@@ -3,31 +3,52 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
-export default [
-  { ignores: ['dist'] },
-  {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
+// export default [
+//   { ignores: ['dist'] },
+//   {
+//     files: ['**/*.{js,jsx}'],
+//     languageOptions: {
+//       ecmaVersion: 2020,
+//       globals: globals.browser,
+//       parserOptions: {
+//         ecmaVersion: 'latest',
+//         ecmaFeatures: { jsx: true },
+//         sourceType: 'module',
+//       },
+//     },
+//     plugins: {
+//       'react-hooks': reactHooks,
+//       'react-refresh': reactRefresh,
+//     },
+//     rules: {
+//       ...js.configs.recommended.rules,
+//       ...reactHooks.configs.recommended.rules,
+//       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+//       'react-refresh/only-export-components': [
+//         'warn',
+//         { allowConstantExport: true },
+//       ],
+//     },
+//   },
+// ]
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
   },
-]
+  extends: [
+    'plugin:react/recommended',
+    'eslint:recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react/react-in-jsx-scope': 'off', // React 17 JSX transform support
+    'react/jsx-uses-react': 'off', // React 17 JSX transform support
+    'react/jsx-uses-vars': 'warn',
+  },
+};
