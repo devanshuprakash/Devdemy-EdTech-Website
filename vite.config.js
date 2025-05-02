4715713107ea1ad3bb07d46d73a21fd87f150236
Devanshu Prakash <devanshuprakash2301@gmail.com>
2025-05-02 17:36:29 +0530

    djjddd

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite';
+// import @tailwindcss;
 
+// export default defineConfig({
+//   plugins: [
+//     react({
+//       include: /\.(mdx|js|jsx|ts|tsx)$/,
+//     }),
+//  , tailwindcss()],
+// });
 
 // https://vite.dev/config/
-export default defineConfig({
+// export default defineConfig({
+//   plugins: [react(),tailwindcss()],
+//   }
+// )
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+export default ({
   plugins: [react(),tailwindcss()],
-  }
-)
+});
